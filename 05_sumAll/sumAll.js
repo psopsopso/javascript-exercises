fcb7d6e8ac88277433616dce10a917d6604a0b16
Patrice Sorrant <48038618+psopsopso@users.noreply.github.com>
2022-09-22 23:52:15 -0400

    Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof(num1) != "number" || typeof(num2) != "number") {
+        return "ERROR";
+    }
+    let sum = 0;
+    lowerBound = Math.min(num1, num2);
+    upperBound = Math.max(num1, num2);
+    for (let i = lowerBound; i <= upperBound; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
