49a543c209746722a79f909e02d6e09607444372
Patrice Sorrant <48038618+psopsopso@users.noreply.github.com>
2022-09-22 22:43:15 -0400

    Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num === 0 || str === '') {
+        return "";
+    }
+    else if (num > 0) {
+        return str.repeat(num);
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
