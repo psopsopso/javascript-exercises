936b9dcecef0ef468e2fef490341a91aa5fe7398
Patrice Sorrant <48038618+psopsopso@users.noreply.github.com>
2022-09-22 23:22:23 -0400

    Solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...item) {
+    for (arg of item) {
+        item_index = arr.indexOf(arg);
+        if (item_index != -1) {
+            arr.splice(item_index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
